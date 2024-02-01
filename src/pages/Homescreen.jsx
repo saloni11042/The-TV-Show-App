08fb7cffb9f87f4370fe2a8a22c783afa6c44d55
saloni11042 <saloni11042@gmail.com>
2024-02-01 17:38:44 +0530

    updated homescreen

--- src/pages/Homescreen.jsx
+++ src/pages/Homescreen.jsx
@@ -1,29 +1,12 @@
-// src/components/ShowList.js
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
+
+import React from "react";
 import Header from "../components/Header";
 import Row from "../components/Row";
 
 function Homescreen(){
-  const [shows, setShows] = useState([]);
-
-  useEffect(() => {
-    const fetchShows = async () => {
-      try {
-        const response = await axios.get("https://api.tvmaze.com/shows");
-        setShows(response.data);
-      } catch (error) {
-        console.error("Error fetching shows:", error);
-      }
-    };
-
-    fetchShows();
-  }, []);
-
   return (
     <div>
-      <Header>HomeScreen</Header>
+      <Header/>
       <div className='container-fluid'>
       <Row genreId={1} genreName="Drama" />
       <Row genreId={2} genreName="Crime" />

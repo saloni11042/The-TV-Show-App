c1fc2cb51b048d9cb7c7bcaceb7b1bf182dca00d
saloni11042 <saloni11042@gmail.com>
2024-02-01 19:35:03 +0530

    adjusting api url

--- src/pages/AllShows.jsx
+++ src/pages/AllShows.jsx
@@ -1,19 +1,17 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-
-function AllShows(){
+function AllShows() {
   const [allShows, setAllShows] = useState([]);
 
   useEffect(() => {
     const fetchAllShows = async () => {
       try {
-       
-        const response = await axios.get('https://api.tvmaze.com/shows');
+        const response = await axios.get("https://api.tvmaze.com/shows");
         setAllShows(response.data);
       } catch (error) {
-        console.error('Error fetching all shows:', error);
+        console.error("Error fetching all shows:", error);
       }
     };
 
@@ -23,23 +21,34 @@ function AllShows(){
   return (
     <div>
       <h2>All Shows</h2>
-      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+        }}
+      >
         {allShows.map((show) => (
-          <div key={show.id} style={{ margin: '20px', maxWidth: '200px' }}>
+          <div key={show.id} style={{ margin: "20px", maxWidth: "200px" }}>
             <Link to={`/show/${show.id}`}>
-              <img src={show.image.medium} alt={show.name} style={{ maxWidth: '100%' }} />
+              <img
+                src={show.image.medium}
+                alt={show.name}
+                style={{ maxWidth: "100%" }}
+              />
             </Link>
-    
+
             <Link to={`/show/${show.id}`}>
-            <p style={{fontSize: '20px'}}>{show.name}</p>
-            
+              <p style={{ fontSize: "20px" }}>{show.name}</p>
             </Link>
-            <p style={{color: 'red', fontSize: '10px'}}>{show.genres.join(', ')}</p>
+            <p style={{ color: "red", fontSize: "10px" }}>
+              {show.genres.join(", ")}
+            </p>
           </div>
         ))}
       </div>
     </div>
   );
-};
+}
 
 export default AllShows;
